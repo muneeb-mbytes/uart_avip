3f70847a9a931610199a97af0208fd44843d596c
Vamshidharreddy <vamshidharreddy@mirafra.com>
2021-11-03 01:34:32 +0530

    added sim folder for compilation and simulation

---
+++ sim/uart_compile.f
@@ -0,0 +1,30 @@
++incdir+../../src/globals/ \
++incdir+../../src/hvl_top/test/sequences/master_sequences/ \
++incdir+../../src/hvl_top/master/ \
++incdir+../../src/hdl_top/master_agent_bfm/  \
++incdir+../../src/hvl_top/env/virtual_sequencer/ \
++incdir+../../src/hvl_top/test/virtual_sequences/ \
++incdir+../../src/hvl_top/env \
++incdir+../../src/hvl_top/slave \
++incdir+../../src/hvl_top/test/sequences/slave_sequences/ \
++incdir+../../src/hvl_top/test \
++incdir+../../src/hdl_top/slave_agent_bfm \
++incdir+../../src/hdl_top/uart_interface \
+../../src/globals/uart_globals_pkg.sv \
+../../src/hvl_top/master/uart_master_pkg.sv \
+../../src/hvl_top/slave/uart_slave_pkg.sv \
+../../src/hvl_top/test/sequences/master_sequences/master_uart_seq_pkg.sv \
+../../src/hvl_top/test/sequences/slave_sequences/slave_uart_seq_pkg.sv \
+../../src/hvl_top/env/uart_env_pkg.sv \
+../../src/hvl_top/test/virtual_sequences/uart_virtual_seq_pkg.sv \
+../../src/hvl_top/test/test_pkg.sv \
+../../src/hdl_top/uart_interface/uart_if.sv \
+../../src/hdl_top/master_agent_bfm/master_driver_bfm.sv \
+../../src/hdl_top/master_agent_bfm/master_monitor_bfm.sv \
+../../src/hdl_top/master_agent_bfm/master_agent_bfm.sv \
+../../src/hdl_top/slave_agent_bfm/slave_driver_bfm.sv \
+../../src/hdl_top/slave_agent_bfm/slave_monitor_bfm.sv \
+../../src/hdl_top/slave_agent_bfm/slave_agent_bfm.sv \
+../../src/hdl_top/hdl_top.sv \
+../../src/hvl_top/hvl_top.sv 
+
