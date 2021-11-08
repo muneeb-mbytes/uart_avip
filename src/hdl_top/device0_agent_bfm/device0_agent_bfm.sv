8d10c4d6d5fc8c7b09c16d9752b8d7661f012197
rakesh271 <rakeshkumar@mirafra.com>
2021-11-08 07:11:07 +0530

    changes made as per coding guidelines

--- src/hdl_top/device0_agent_bfm/device0_agent_bfm.sv
+++ src/hdl_top/device0_agent_bfm/device0_agent_bfm.sv
@@ -5,24 +5,26 @@
 // Module: device0 Agent BFM
 // This module is used as the configuration class for device0 agent bfm and its components
 //--------------------------------------------------------------------------------------------
-
 module device0_agent_bfm(uart_if intf);
+    
+  //-------------------------------------------------------
+  // Package : Importing Uvm Pakckage and Test Package
+  //-------------------------------------------------------
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
 
- import uvm_pkg::*;
-`include "uvm_macros.svh"
-
- initial
- begin
-    $display("device0 Agent BFM");
- end
+  initial
+  begin
+     $display("device0 Agent BFM");
+  end
+  
+  device0_driver_bfm device0_drv_bfm_h(intf);
+  device0_monitor_bfm device0_mon_bfm_h(intf);
  
- device0_driver_bfm device0_drv_bfm_h(intf);
- device0_monitor_bfm device0_mon_bfm_h(intf);
-
- //-------------------------------------------------------
- // Setting the virtual handle of BMFs into config_db
- //-------------------------------------------------------
- initial begin
+  //-------------------------------------------------------
+  // Setting the virtual handle of BMFs into config_db
+  //-------------------------------------------------------
+  initial begin
    uvm_config_db#(virtual device0_driver_bfm)::set(null,"*", "device0_driver_bfm", device0_drv_bfm_h); 
    uvm_config_db#(virtual device0_monitor_bfm)::set(null,"*", "device0_monitor_bfm", device0_mon_bfm_h);
  end
