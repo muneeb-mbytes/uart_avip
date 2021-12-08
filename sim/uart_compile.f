f7fff3bc60f4ac15614d8f1a2e35004ed8ed47d9
Vamshidharreddy <vamshidharreddy@mirafra.com>
2021-12-08 10:15:08 +0530

    modified folder device agent to device

--- sim/uart_compile.f
+++ sim/uart_compile.f
@@ -3,8 +3,8 @@
 +incdir+../../src/hvl_top/device/device_rx/ 
 +incdir+../../src/hvl_top/device/device_tx/
 +incdir+../../src/hvl_top/device/
-+incdir+../../src/hvl_top/device/device_agent/
-+incdir+../../src/hdl_top/device/Device_agent_bfm/  
++incdir+../../src/hvl_top/device/device/
++incdir+../../src/hdl_top/device/device_agent_bfm/  
 +incdir+../../src/hvl_top/env/virtual_sequencer/ 
 +incdir+../../src/hvl_top/test/virtual_sequences/ 
 +incdir+../../src/hvl_top/env/ 
@@ -15,7 +15,7 @@
 ../../src/hvl_top/device/device_rx/rx_pkg.sv
 ../../src/hvl_top/test/sequences/device_sequences/tx_sequences/tx_uart_seq_pkg.sv
 ../../src/hvl_top/test/sequences/device_sequences/rx_sequences/rx_uart_seq_pkg.sv
-../../src/hvl_top/device/device_agent/device_pkg.sv
+../../src/hvl_top/device/device/device_pkg.sv
 ../../src/hvl_top/env/uart_env_pkg.sv
 ../../src/hvl_top/test/virtual_sequences/uart_virtual_seq_pkg.sv
 ../../src/hvl_top/test/test_pkg.sv
